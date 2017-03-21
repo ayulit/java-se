b0bf53ce141d4f739ae03d4ff09e806f8fbd06f6
ayulit <litwinoff@gmail.com>
2017-03-21 14:07:22 +0300

    CJ-v1cg05: equals() for subclasss

---
+++ core-java-book/src/main/java/volume1/chapter05/inheritance/object/Manager.java
@@ -0,0 +1,33 @@
+package volume1.chapter05.inheritance.object;
+
+public class Manager extends Employee
+{
+   private double bonus;
+
+   public Manager(String n, double s, int year, int month, int day)
+   {
+      super(n, s, year, month, day);
+      bonus = 0;
+   }
+
+   public double getSalary()
+   {
+      double baseSalary = super.getSalary();
+      return baseSalary + bonus;
+   }
+
+   public void setBonus(double b)
+   {
+      bonus = b;
+   }
+
+   public boolean equals(Object otherObject)
+   {
+      if (!super.equals(otherObject)) return false;
+      Manager other = (Manager) otherObject;
+      // super.equals checked that this and other belong to the same class
+      return bonus == other.bonus;
+   }
+
+}
+
